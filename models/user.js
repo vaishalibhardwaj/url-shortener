467b75abb2d01f597fcef37edc8b5647cc6d3f01
Vaishali Bhardwaj <47493831+vaishalibhardwaj@users.noreply.github.com>
2024-07-01 18:43:25 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
