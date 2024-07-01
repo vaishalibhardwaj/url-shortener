ee61e211f38c0c2d715da8d6bce65f47caae867c
Vaishali Bhardwaj <47493831+vaishalibhardwaj@users.noreply.github.com>
2024-07-01 18:40:57 +0530

    Add files via upload

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const User = require('../models/user');
+
+const registerUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+
+  try {
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Could not register user' });
+  }
+};
+
+module.exports = { registerUser };
